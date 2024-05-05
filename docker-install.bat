39638a3bb22c732504fb653f2dd3c95800fce2a0
Anderson0Wagner <anderson.wagner@gmail.com>
2024-05-05 13:21:57 -0300

    alterações para iniciar o microserviço de pedido com docker compose

--- docker-install.bat
+++ docker-install.bat
@@ -1,2 +1,2 @@
 docker pull postgres:latest
-docker run --name mappin-pedido-db -p 5434:5432 -e POSTGRES_USER=mappin -e POSTGRES_PASSWORD=mappinPedido -e POSTGRES_DB=mappin-pedido-db -d postgres
+docker run --name mappin-standalone-pedido-db -p 5434:5432 -e POSTGRES_USER=mappin -e POSTGRES_PASSWORD=mappinPedido -e POSTGRES_DB=mappin-pedido-db -d postgres
